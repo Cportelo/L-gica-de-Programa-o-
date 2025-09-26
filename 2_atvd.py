38b55c231bfbd4536a38161f0eeebf809c09ac2d
Cportelo <cristianoportelo2016@gmail.com>
2025-09-26 16:47:42 -0300

    Add files via upload

---
+++ 2_atvd.py
@@ -0,0 +1,38 @@
+import os
+os.system("cls")
+
+
+
+qntd = 0
+pares = 0
+impares = 0
+qntd_par = 0
+qntd_impar = 0
+
+while True:
+    numeros = int(input("Digite um número: "))
+    qntd = qntd + 1
+
+    if numeros % 2 == 0:  
+        qntd_par += 1
+        pares = pares + 1
+    else: 
+        qntd_impar += 1
+        impares = impares + 1
+
+    if qntd > 3: 
+        norma = str(input("Digite S para continuar e N para parar (S/N): " )).upper
+        
+        if norma == "N":
+            print("Calculando...")
+            break
+
+media_par = pares / qntd_par if qntd_par != 0 else 0
+media_impar = impares / qntd_impar if qntd_impar != 0 else 0
+media_geral = (pares + impares) / qntd
+
+print(f"Sua quantidade de numeros pares: {qntd_par: .2f}")
+print(f"Sua quantidade de numeros ímpares: {qntd_impar: .2f}")
+print(f"a média dos numeros é: {media_geral: .2f}")
+print(f"a média dos pares: {media_par: .2f}")
+
---
+++ 4_atvd.py
@@ -0,0 +1,34 @@
+import os
+os.system
+
+while True:
+
+ total_familia = 0
+ total_filhos= 0
+ maior_salario = 0
+ menor_salario = 0
+
+
+
+
+while True:
+    os.system("cls")
+    print("""
+    código | Descrição
+    1  |Adicionar pessoas
+    2  |Exibir resultados e sair 
+  
+""")
+    
+
+
+    match codígo:
+       
+        case 1:
+          total_familia = int(input("Digite quantidade "))
+
+
+
+
+   
+
